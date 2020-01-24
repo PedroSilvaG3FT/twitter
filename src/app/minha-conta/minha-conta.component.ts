6b676843c8062729df872f980fb2891bb55d8097
pedro.silva <walme@LAPTOP-13PJ4Q0V>
2020-01-24 18:02:48 -0300

    Iniciando areas de cards

--- src/app/minha-conta/minha-conta.component.ts
+++ src/app/minha-conta/minha-conta.component.ts
@@ -6,10 +6,103 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./minha-conta.component.scss']
 })
 export class MinhaContaComponent implements OnInit {
-
+  public lstDependentes = listTest;
   constructor() { }
 
   ngOnInit() {
+    console.log("DEPENDENTES", this.lstDependentes)
   }
 
 }
+
+export const listTest = [
+  {
+    nome: "Nome Teste",
+    cpf: "496.333.518-20",
+    email:"email@teste.com",
+    telefone: "(11) 95888-5825",
+    grauParentesco: "Filho",
+    sexo: "Masculino",
+    dataNascimento: new Date(),
+    status: "Aprovado"
+  },
+
+  {
+    nome: "Nome Teste",
+    cpf: "496.333.518-20",
+    email: "email@teste.com",
+    telefone: "(11) 95888-5825",
+    grauParentesco: "Filho",
+    sexo: "Masculino",
+    dataNascimento: new Date(),
+    status: "Aprovado"
+  },
+
+  {
+    nome: "Nome Teste",
+    cpf: "496.333.518-20",
+    email: "email@teste.com",
+    telefone: "(11) 95888-5825",
+    grauParentesco: "Filho",
+    sexo: "Masculino",
+    dataNascimento: new Date(),
+    status: "Aprovado"
+  },
+
+  {
+    nome: "Nome Teste",
+    cpf: "496.333.518-20",
+    email: "email@teste.com",
+    telefone: "(11) 95888-5825",
+    grauParentesco: "Filho",
+    sexo: "Masculino",
+    dataNascimento: new Date(),
+    status: "Aprovado"
+  },
+
+  {
+    nome: "Nome Teste",
+    cpf: "496.333.518-20",
+    email: "email@teste.com",
+    telefone: "(11) 95888-5825",
+    grauParentesco: "Filho",
+    sexo: "Masculino",
+    dataNascimento: new Date(),
+    status: "Aprovado"
+  },
+
+  {
+    nome: "Nome Teste",
+    cpf: "496.333.518-20",
+    email: "email@teste.com",
+    telefone: "(11) 95888-5825",
+    grauParentesco: "Filho",
+    sexo: "Masculino",
+    dataNascimento: new Date(),
+    status: "Aprovado"
+  },
+
+  {
+    nome: "Nome Teste",
+    cpf: "496.333.518-20",
+    email: "email@teste.com",
+    telefone: "(11) 95888-5825",
+    grauParentesco: "Filho",
+    sexo: "Masculino",
+    dataNascimento: new Date(),
+    status: "Aprovado"
+  },
+
+  {
+    nome: "Nome Teste",
+    cpf: "496.333.518-20",
+    email: "email@teste.com",
+    telefone: "(11) 95888-5825",
+    grauParentesco: "Filho",
+    sexo: "Masculino",
+    dataNascimento: new Date(),
+    status: "Aprovado"
+  },
+
+  
+]
