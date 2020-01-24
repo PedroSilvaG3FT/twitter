b1650a51e247bcd67ea1f4614b5b5de4462db979
pedro.silva <walme@LAPTOP-13PJ4Q0V>
2020-01-24 10:32:16 -0300

    Iniciando Nav Bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CollapseModule, BsDropdownModule } from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+
+    CollapseModule.forRoot(), 
+    BsDropdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
